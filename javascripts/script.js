2e436da363e6a9b01c9231c58c1c74748aa32a2e
Ney Ricardo <hello@neyricardo.com>
2013-09-29 21:34:57 -0300

    Home finalizada

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,8 +2,11 @@
 
 */
 
+$(".chosen-select").chosen();
 
-
+$(".home-search-estado").chosen({width: "130px"});
+$(".home-search-cidade").chosen({width: "260px"});
+$(".home-search-finalidade").chosen({width: "180px"});
 
 
 
